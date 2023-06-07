6469ce012087c7436ac093de6ba11f9467d53f24
Naveen-Ojha <naveen.ojha.rewa@gmail.com>
2023-06-08 00:31:57 +0530

    production url change

--- src/enviroment.js
+++ src/enviroment.js
@@ -1,1 +1,2 @@
-export const apiEndPoint = 'http://localhost:8080/api/'
+// export const apiEndPoint = 'http://localhost:8080/api/'
+export const apiEndPoint = 'https://portfoliobackend-duyx.onrender.com/'
